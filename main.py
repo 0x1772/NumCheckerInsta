84c4da82eaa8cb70bec9cc9711a9c99b11838ef7
0X1772 <fakesmileux@gmail.com>
2023-10-21 16:19:04 +0300

    valid_check

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import re
 import requests
+
+def is_valid_phone_number(phone_number):
+    # Use regular expression to check if phone number is valid
+    pattern = re.compile(r'^\+?\d{10,12}$')
+    return pattern.match(phone_number)
+
